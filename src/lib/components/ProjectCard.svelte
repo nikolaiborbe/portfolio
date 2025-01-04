fc8c8ec99b1ea8e068f8d8b2bcac5d82d6c94f86
nikolaiborbe <nikolaiborbe@gmail.com>
2025-01-04 16:42:16 +0100

    started project card

--- src/lib/components/ProjectCard.svelte
+++ src/lib/components/ProjectCard.svelte
@@ -1,12 +1,18 @@
 <script lang="ts">
-  let {title, description, image, tags, link, github} = $props();
+	let { title, description, image, tags, link, github } = $props();
 </script>
 
-<div>{title}</div>
-<div>{description}</div>
-<div>{image}</div>
-{#each tags as tag}
-  <div>{tag}</div>
-{/each}
-<div>{link}</div>
-<div>{github}</div>
+<div class="flex flex-col md:flex-row rounded-md border border-[#3F3F46] p-2">
+	<div class="picture">
+    <img src={image} alt="Daily crimes" class="rounded-md " />
+	</div>
+	<div class="content">
+		<div>{title}</div>
+		<div>{description}</div>
+		{#each tags as tag}
+			<div>{tag}</div>
+		{/each}
+		<div>{link}</div>
+		<div>{github}</div>
+	</div>
+</div>
