e7fb114978b036544bd512cd07a6fd0acb4b532a
Nikolai-Grytvik-Borbe <nikolaiborbe@gmail.com>
2025-02-13 18:49:25 +0100

    add loading to picture (too high quality)

--- src/lib/components/UserCard.svelte
+++ src/lib/components/UserCard.svelte
@@ -3,9 +3,14 @@
   import BuyCoffee from "$lib/icons/BuyCoffee.svelte";
 	import GitHub from "$lib/icons/GitHub.svelte";
 	import LeetCode from "$lib/icons/LeetCode.svelte";
+
+  let loading = $state(true);
 </script>
 <div class="flex items-center ">
-  <img src="pp.png" class="w-32 h-32 md:w-40 md:h-40 rounded-lg" alt="" />
+  {#if loading}
+    <div class="absolute animate-pulse w-32 h-32 md:w-40 md:h-40 rounded-lg bg-black"></div>
+  {/if}
+  <img src="pp.png" onload={() => loading = false} class="w-32 h-32 md:w-40 md:h-40 rounded-lg" alt="" />
   <div class="pl-4 flex flex-col gap-1 md:gap-2 ">
     <h1 class="font-bold text-2xl md:text-4xl">Nikolai</h1>
     <h2 class="md:text-xl">Average programming enjoyer</h2>
