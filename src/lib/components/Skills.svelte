65acc4eeb1bc1bfe60f6c845368531424ebca119
Nikolai-Grytvik-Borbe <nikolaiborbe@gmail.com>
2025-08-04 16:57:45 +0200

    update skills

--- src/lib/components/Skills.svelte
+++ src/lib/components/Skills.svelte
@@ -2,30 +2,31 @@
 	import Skill from '$lib/icons/Skill.svelte';
 	import Skill2 from '$lib/icons/Skill2.svelte';
 	import C from '$lib/icons/C.svelte';
+	import OCaml from '$lib/icons/OCaml.svelte';
 	import Rust from '$lib/icons/Rust.svelte';
 
 	import {
 		LogoSvelte,
 		LogoPython,
-		Sql,
 	} from 'carbon-icons-svelte';
 
 	let skills = [
 		'Python',
 		'TypeScript',
-		'React',
 		'git',
     "Tailwind-css",
 	];
 	let skills2: { [key: string]: typeof LogoPython } = {
 		Svelte: LogoSvelte,
-		Sql: Sql,
 	};
 </script>
 
 <div class="flex flex-wrap gap-2">
   <div class="flex h-fit w-fit items-center gap-2 rounded-md border border-[#3F3F46] bg-[#212123] px-3 py-1">
-    <div class="scale-125"><C /></div> C
+		<div class="scale-125"><C /></div> C
+  </div>
+  <div class="flex h-fit w-fit items-center gap-2 rounded-md border border-[#3F3F46] bg-[#212123] px-3 py-1">
+		<div class="scale-125"><OCaml /></div> OCaml
   </div>
   <div class="flex h-fit w-fit items-center gap-2 rounded-md border border-[#3F3F46] bg-[#212123] px-3 py-1">
 		<div class=""><Rust /></div> Rust
