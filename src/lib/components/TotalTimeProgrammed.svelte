dee393ef6d145e7181fba7e66e64d78325da4f10
Nikolai-Grytvik-Borbe <nikolaiborbe@gmail.com>
2025-09-02 23:09:19 +0200

    add not tracked estimated time to make total time more realistic

--- src/lib/components/TotalTimeProgrammed.svelte
+++ src/lib/components/TotalTimeProgrammed.svelte
@@ -5,13 +5,14 @@
 		const res = await fetch('/api/tot-time');
 		return await res.json();
 	}
+	let not_tracked_estimated_time = 750;
 	let totalTime = $state(0);
 	let loading = $state(true);
 	onMount(async () => {
 		const data = await getTime();
-		totalTime = Math.floor(data.seconds / 3600);
+		totalTime = Math.floor(data.seconds / 3600) + not_tracked_estimated_time;
 		loading = false;
 	});
 </script>
 
-{totalTime ? totalTime + ' hours' : ''}
+{totalTime ? totalTime + ' hours total' : ''}
