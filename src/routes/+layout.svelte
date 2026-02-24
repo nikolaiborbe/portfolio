8a45b67e787888a67a661473f322cbb4926e56bb
Nikolai Grytvik Borbe <nikolaiborbe@gmail.com>
2026-02-24 15:32:01 +0100

    bytt font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,6 +14,6 @@
 	<meta property="og:type" content="website" />
 </svelte:head>
 
-<div class="font-mono p-4 mx-auto max-w-[750px]">
+<div class="font-serif p-4 mx-auto max-w-[750px]">
 	{@render children()}
 </div>
