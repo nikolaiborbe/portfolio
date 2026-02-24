e142dfe59ccea07a6597576883c34e594865d013
Nikolai Grytvik Borbe <nikolaiborbe@gmail.com>
2026-02-24 20:58:59 +0100

    add some books

---
+++ src/routes/books/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+</script>
+
+<h1 class="text-4xl">Books</h1>
+
+<br>
+
+<div class="pl-4">
+	<ul class="list-disc">
+		<li>
+			<a href="/books/introduction_to_optics.pdf" class="text-fg-brand underline hover:no-underline" target="_blank">Introduction To Optics</a>
+		</li>
+		<li>
+			<a href="/books/rocket_propulsion_elements.pdf" class="text-fg-brand underline hover:no-underline" target="_blank">Rocket Propulsion Elements</a>
+		</li>
+		<li>
+			<a href="/books/quantum_mechanics_3rd.pdf" class="text-fg-brand underline hover:no-underline" target="_blank">Quantum Mechanics (3rd edition)</a>
+		</li>
+		<li>
+			<a href="/books/linear_algebra.pdf" class="text-fg-brand underline hover:no-underline" target="_blank">Linear Algebra</a>
+		</li>
+	</ul>
+</div>
