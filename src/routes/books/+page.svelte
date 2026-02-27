94f04a4852dc526ad55d08856ae7419ff846e465
Nikolai Grytvik Borbe <nikolaiborbe@gmail.com>
2026-02-27 15:47:15 +0100

    add a few new textbooks

--- src/routes/books/+page.svelte
+++ src/routes/books/+page.svelte
@@ -19,5 +19,14 @@
 		<li>
 			<a href="/books/linear_algebra.pdf" class="text-fg-brand underline hover:no-underline" target="_blank">Linear Algebra</a>
 		</li>
+		<li>
+			<a href="/books/advanced_engineering_mathematics.pdf" class="text-fg-brand underline hover:no-underline" target="_blank">Advanced Engineering Mathematics</a>
+		</li>
+		<li>
+			<a href="/books/elementary_linear_algebra_applications.pdf" class="text-fg-brand underline hover:no-underline" target="_blank">Elementary Linear Algebra Applications</a>
+		</li>
+		<li>
+			<a href="/books/probability_and_statistics_for_engineers.pdf" class="text-fg-brand underline hover:no-underline" target="_blank">Probability And Statistics For Engineers</a>
+		</li>
 	</ul>
 </div>
